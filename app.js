37d0dedba971564854c18fb116463fe92305c1ae
GustaveNimant <achadde@iabloko.fecamp>
2019-06-27 11:22:32 +0200

    fonctionne avec app.js + index.html

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-var express = require("express");
+const express = require("express");
+const mongoose = require('mongoose');
+
 var app = express();
 var port = 3000;
- 
-app.get("/", (req, res) => {  /* app.use equivalent ? */
- res.send("Hello World");
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost:27017/node-demo", { useNewUrlParser: true });
+
+const nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastNameName: String
+});
+
+const User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+ res.sendFile(__dirname + "/index.html");
 });
  
+app.post("/addname", (req, res) => {
+    const myData = new User(req.body);
+    myData.save()
+	.then(item => {
+	    res.send("item saved to database");
+	})
+	.catch(err => {
+	    res.status(400).send("unable to save to database");
+	});
+});
+
 app.listen(port, () => {
  console.log("Server listening on http://localhost:" + port);
 });
+
