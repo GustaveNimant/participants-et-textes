70539704fc4ae6baa1addffc3bd56f8d26434410
GustaveNimant <achadde@iabloko.fecamp>
2019-06-29 16:35:40 +0200

    Lecteur et Livres dans node-demo-db

--- app.js
+++ app.js
@@ -1,52 +1,86 @@
 const express = require("express");
-const mongoose = require('mongoose');
-
 const app = express();
-const port = 3000;
+
+const port_server = 3000;
+const name_db = "node-demo-db";
+const port_db = 27017;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const name_db = "node-demo_db";
-const port_db = "27017";
-mongoose.connect("mongodb://localhost:" + port_db + "/" + name_db, { useNewUrlParser: true });
-
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:" + port_db + "/" + name_db);
 
-const nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
-},{
-    collection : 'user_c'
-});
-
-const User = mongoose.model("User", nameSchema); /* mongoose document - database */
-
-/* display form page */
 app.get("/", (req, res) => {
-    const fullNameOfFile = __dirname + "/index.html";
-    console.log ('app.get file full path is', fullNameOfFile);
-    res.sendFile(fullNameOfFile);
+    res.sendFile(__dirname + "/index.html");
 });
 
-/* get firstName and lastName from form as req.body */
-/* save them in database with res.send              */
-/* <form method="post" action="/addname">           */
-app.post("/addname", (req, res) => {
-    console.log('app.post route is', req.route);
-    const myData = new User(req.body);
+const inscriptionSchema = new mongoose.Schema({
+    email: String,
+    pseudo: String,
+    password : String
+});
+
+const Lecteur = mongoose.model("Lecteur", inscriptionSchema);
+
+app.get("/inscription", (req, res) => {
+    res.sendFile(__dirname + "/inscription.html");
+});
+
+app.post("/nouveau-lecteur", (req, res) => {
+    var myData = new Lecteur(req.body);
     myData.save()
-	.then(item => {
-	    res.send("<h3>Data saved to database <i>"+name_db+"</i></h3>");
-	})
-	.catch(err => {
-	    console.log('app.post save error is', err);
-	    res.status(400).send("unable to save to database");
-	});
+        .then(item => {
+            res.send('Inscription OK <a href="/">Retour a l\'accueil</a>');
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
 });
 
-app.listen(port, () => {
-    console.log("Server listening on http://localhost:" + port);
+app.get("/un-lecteur", (req, res) => {
+    res.sendFile(__dirname + "/un-lecteur.html");
 });
 
+const inscriptionModel = mongoose.model('Lecteur', inscriptionSchema);
+
+const query = inscriptionModel.find();
+query.where('pseudo', 'Julien');
+query.limit(3);
+
+query.exec(function (err, the_inscriptions) {
+    if (err) { throw err; }
+    var an_inscription;
+    console.log('length',the_inscriptions.length);
+    
+    for (var i = 0, l = the_inscriptions.length; i < l; i++) {
+	an_inscription = the_inscriptions[i];
+	
+	console.log('------------------------------');
+	console.log('Pseudo : ' + an_inscription.pseudo);
+    }
+});
+
+const livreSchema = new mongoose.Schema({
+    titreLivre: String,
+    auteurLivre: String
+});
+
+const Livre = mongoose.model("Livre", livreSchema);
+
+app.post("/nouveau-livre", (req, res) => {
+    var myData = new Livre(req.body);
+    myData.save()
+        .then(item => {
+            res.send('Name saved to database <a href="/">Retour a l\'accueil</a>');
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port_server, () => {
+    console.log("Server listening on port http://localhost:" + port_server);
+});
