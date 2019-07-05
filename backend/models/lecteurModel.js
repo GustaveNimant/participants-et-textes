363648c99d405476ec47732412fa2323dd624025
GustaveNimant <achadde@iabloko.fecamp>
2019-07-05 17:07:32 +0200

    backend/models/lecteurModel.js corrigé

--- backend/models/lecteurModel.js
+++ backend/models/lecteurModel.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //const uniqueValidator = require('mongoose-unique-validator');
     
-const userSchema = new Schema({
-    email: { type: String },
-    pseudo: { type: String },
-    password: { type: String }
+const lecteurSchema = new Schema({
+    email: { type: String, required: true },
+    pseudo: { type: String, required: true },
+    password: { type: String, required: true }
 },{
     collection : 'lecteur_c'			    
 });
 
-//userSchema.plugin(uniqueValidator);
+//lecteurSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('userModel', userSchema);
+module.exports = mongoose.model('lecteurModel', lecteurSchema);
